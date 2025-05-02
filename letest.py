2501667b089cad1d65052885c19089c6015ba9d1
souravddlop <nallabotooo@gmail.com>
2025-05-02 10:16:14 +0530

    Update letest.py

--- letest.py
+++ letest.py
@@ -88,7 +88,7 @@ def handle_attack(message):
     command = message.text.split()
 
 if message.chat.id != int(GROUP_ID):
-        bot.reply_to(message, "🚫 𝐘𝐄 𝐁𝐎𝐓 𝐒𝐈𝐑𝐅 𝐆𝐑𝐎𝐔𝐏 𝐌𝐄 𝐂𝐇𝐀𝐋𝐄𝐆𝐀 ❌\n🔗 𝐉𝐨𝐢𝐧 𝐍𝐨𝐖: https://t.me/+ZOwep4Yba59hYmI1"
+        bot.reply_to(message, "🚫 𝐘𝐄 𝐁𝐎𝐓 𝐒𝐈𝐑𝐅 𝐆𝐑𝐎𝐔𝐏 𝐌𝐄 𝐂𝐇𝐀𝐋𝐄𝐆𝐀 ❌\n🔗 𝐉𝐨𝐢𝐧 𝐍𝐨𝐖:t.me/+ZOwep4Yba59hYmI1"
             return
 
 
